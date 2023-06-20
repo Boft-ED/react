5a2c51d40ef37ec1e61ad7b007f3601af92f694c
Nasekin <nasekinwhiteorblack@gmail.com>
2023-06-20 22:08:55 +0300

    разбил на компоненты

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,26 +1,38 @@
-function PizzaBlock(props) {
+import { useState } from "react";
+
+function PizzaBlock({ title, price, imageUrl, sizes, types }) { // деструктуризация 
+    const [pizzaCount, setPizzaCount] = useState(0); // первый аргумент записывает в себя изменения, которые происходят во 2 аргументе и 2 аргумент заставляет перерисоваться компонент 
+    const onClickAdd = () => {
+        setPizzaCount(pizzaCount + 1)
+    };
+
+    const typePizza = ['традицонное', 'тонкое'];
+    const [activeType, setactiveType] = useState(0);
+    const [activeSize, setactiveSize] = useState(0);
+
     return (
-        <div class="pizza-block">
+        <div className="pizza-block">
             <img
-                class="pizza-block__image"
-                src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+                className="pizza-block__image"
+                src={imageUrl}
                 alt="Pizza"
             />
-            <h4 class="pizza-block__title">{props.title}</h4>
-            <div class="pizza-block__selector">
+            <h4 className="pizza-block__title">{title}</h4>
+            <div className="pizza-block__selector">
                 <ul>
-                    <li class="active">тонкое</li>
-                    <li>традиционное</li>
+                    {types.map(type =>
+                        <li onClick={() => setactiveType(type)} className={activeType === type ? 'active' : ''}>{typePizza[type]}</li>
+                    )}
                 </ul>
                 <ul>
-                    <li class="active">26 см.</li>
-                    <li>30 см.</li>
-                    <li>40 см.</li>
+                    {sizes.map((size, i) =>
+                        <li onClick={() => setactiveSize(i)} className={activeSize === i ? 'active' : ''}>{size} см.</li>
+                    )}
                 </ul>
             </div>
-            <div class="pizza-block__bottom">
-                <div class="pizza-block__price">от {props.price} ₽</div>
-                <div class="button button--outline button--add">
+            <div className="pizza-block__bottom">
+                <div className="pizza-block__price">от {price} ₽</div>
+                <button onClick={onClickAdd} className="button button--outline button--add">
                     <svg
                         width="12"
                         height="12"
@@ -34,8 +46,8 @@ function PizzaBlock(props) {
                         />
                     </svg>
                     <span>Добавить</span>
-                    <i>2</i>
-                </div>
+                    <i>{pizzaCount}</i>
+                </button>
             </div>
         </div>
     );
