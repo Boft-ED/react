742f2ac7af4320201bfbd806b46c70b62e285132
Nasekin <nasekinwhiteorblack@gmail.com>
2023-06-23 15:31:10 +0300

    сделал пагинацию и изменил поиск с обычного реакт взаим на обращение к бэку

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react';
 
+
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/skeleton';
+import Pagination from '../components/Pagination';
 
 const Home = ({ searchValue }) => {
     const [items, setItems] = useState([]);
     const [isloading, setisLoading] = useState(true);
     // хук, который предназначен, чтобы выполнить код после отрисовки, тк есть жизненный цикл компонента
     const [categoryId, setCategoryId] = useState(0) // родителские стэйты
+    const [pagePagination, setPagePagination] = useState(1);
     const [sort, setSort] = useState({
         name: 'популярности',
         sortProperty: 'raiting'
@@ -22,8 +25,9 @@ const Home = ({ searchValue }) => {
         const sortBy = sort.sortProperty.replace('-', '');
         const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
         const category = categoryId > 0 ? `category=${categoryId}` : '';
+        const search = searchValue ? `&search=${searchValue}` : '';
 
-        fetch(`https://64920b3c2f2c7ee6c2c9570b.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,)
+        fetch(`https://64920b3c2f2c7ee6c2c9570b.mockapi.io/items?page=${pagePagination}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,)
             .then((res) => {
                 return res.json(); // конвертим данные в понятный для реакта вид, полученные с бека
             })
@@ -32,14 +36,9 @@ const Home = ({ searchValue }) => {
                 setisLoading(false);
             });
         window.scrollTo(0, 0);
-    }, [categoryId, sort]); // зависимость от которой будет делаться запрос, если один из параметров изменится 
+    }, [categoryId, sort, searchValue, pagePagination]); // зависимость от которой будет делаться запрос, если один из параметров изменится 
 
-    const pizzas = items.filter((obj) => {
-        if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-            return true;
-        }
-        return false;
-    }).map((obj) => <PizzaBlock key={obj.id}{...obj} />);
+    const pizzas = items.map((obj) => <PizzaBlock key={obj.id}{...obj} />);
     const sceletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
     return (
         <div className="container">
@@ -53,6 +52,7 @@ const Home = ({ searchValue }) => {
                 {isloading ? sceletons : pizzas}
             </div>
             {/* если идет загрузка, то мы создаем фейк массив из 6 состовляющих и превращаем его в массив скелетонов, а когда условие ложно, то загрузка кончилась и мы отрисовываем новый массив с полученными данными от бека */}
+            <Pagination onChangePage={number => setPagePagination(number)} />
         </div>
 
     );
